0dcc6436b5b28e231977a58490ff14aa193ce344
horatio <horatiorosa@aol.com>
2020-09-18 14:15:08 -0400

    much async await work

--- exercises/72 - Async Prompts/scripts.js
+++ exercises/72 - Async Prompts/scripts.js
@@ -5,97 +5,162 @@ function wait(ms = 0) {
 async function destroyPopup(popup) {
   popup.classList.remove('open');
   await wait(1000);
-  // remove the popup entirely!
+  // remove popup entirely
+  // popup.parentElement.removeChild(popup); old way
   popup.remove();
-  /* eslint-disable no-param-reassign */
+  // console.log(popup); // popup still in memory so need to do this:
   popup = null;
-  /* eslint-enable no-param-reassign */
+  // console.log(popup);
 }
 
 function ask(options) {
-  return new Promise(async function(resolve) {
-    // First we need to create a popup with all the fields in it
-    const popup = document.createElement('form');
+  return new Promise(async function(resolve) { // async on parent func.
+    // need to create a popup with all the fields in it
+    const popup = document.createElement('form'); // createElement immediately returns a DOM node that we can listen for events on, can't do the same with backticks wrapping form tag markup
     popup.classList.add('popup');
-    popup.insertAdjacentHTML(
-      'afterbegin',
-      `<fieldset>
-        <label>${options.title}</label>
-        <input type="text" name="input"/>
+    popup.insertAdjacentHTML('afterbegin',
+     `<fieldset>
+        <label> ${options.title} </label>
+        <input type="text" name="input"></input>
         <button type="submit">Submit</button>
-      </fieldset>
-    `
+      </fieldset>`
     );
 
     // check if they want a cancel button
     if (options.cancel) {
       const skipButton = document.createElement('button');
-      skipButton.type = 'button';
+      skipButton.type = 'button'; // if not specified, browser assume this is a submit button
       skipButton.textContent = 'Cancel';
-      console.log(popup.firstChild);
+      // listen for a click on that cancel button
+      function cancel() {
+        resolve(null);
+        destroyPopup(popup);
+      }
+
       popup.firstElementChild.appendChild(skipButton);
-      // TODO: listen for a click on that cancel button
       skipButton.addEventListener(
         'click',
-        function() {
-          resolve(null);
-          destroyPopup(popup);
-        },
-        { once: true }
+        cancel,
+        {once: true}
       );
     }
-    // listen for the submit event on the inputs
+
+    // listen for a submit event on the inputs
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log('Submitted');
+      // when someone does submit the form, resolve the data that was in the input box (e.target gets us the form, e.target.input gets us `name="input"`)
+      resolve(e.target.input.value);
+      destroyPopup(popup);
+    }
+
     popup.addEventListener(
       'submit',
-      function(e) {
-        e.preventDefault();
-        console.log('SUBMITTED');
-        resolve(e.target.input.value);
-        // remove it from the DOM entirely
-        destroyPopup(popup);
-      },
-      { once: true }
+      handleSubmit,
+      {once: true}  // run eventListener once for submit
     );
-    // when someone does submit it, resolve the data that was in the input box!
+
 
     // insert that popup into the DOM
     document.body.appendChild(popup);
-    // put a very small timeout before we add the open class
-
+    // put a very small timeout before we add the open class so the popup has time to be created and then use the CSS transition we want
     await wait(50);
     popup.classList.add('open');
   });
 }
 
-// select all button that have a question
+// select all buttons that have a question
 async function askQuestion(e) {
   const button = e.currentTarget;
   const cancel = 'cancel' in button.dataset;
-
+  /*
+    this is how to check is a property exists in an object
+    const jiro = {dog: true, name: 'jiro'}
+    # undefined
+    jiro
+    # {dog: true, name: "jiro"}
+    'dog' in jiro
+    # true
+  */
+  // alt method: const cancel = button.hasAttribute('data-cancel');
   const answer = await ask({
     title: button.dataset.question,
+    // cancel: cancel, shorthand below
     cancel,
   });
   console.log(answer);
 }
 
 const buttons = document.querySelectorAll('[data-question]');
-buttons.forEach(button => button.addEventListener('click', askQuestion));
+// console.log(buttons);
+buttons.forEach(
+  button => button.addEventListener(
+    'click',
+    askQuestion
+  )
+);
 
 const questions = [
-  { title: 'What is your name?' },
-  { title: 'What is your age?', cancel: true },
-  { title: 'What is your dogs name?' },
+  {title: 'What is your name'},
+  {title: 'What is your age', cancel: true},
+  {title: 'What is your dog\'s name'},
 ];
 
+/*
+ *** this soution fires off all 3 questions concurrently and opens 3 popups! NG
+  const answers = Promise.all([
+    ask(questions[0]),
+    ask(questions[1]),
+    ask(questions[2]),
+  ])
+  .then(
+    answers => {
+      console.log(answers);
+    }
+  );
+
+  REFACTOR ABOVE
+  const queuePromises = questions.map(ask)
+  console.log(queuePromises)
+  # (3) [Promise, Promise, Promise]
+    0: Promise {<pending>}
+    1: Promise {<pending>}
+    2: Promise {<pending>}
+    length: 3
+    __proto__: Array(0)
+
+  Promise.all(questions.map(ask)).then(data => console.log(data));
+  # (3) ["cat", "200", "Jiro"]
+
+*** this soution also fires off all 3 questions concurrently and opens 3 popups! NG
+questions.forEach(async function(question) {
+  const answer = await ask(question);
+  console.log(answer);
+});
+*/
+
+/*
+// unlike `map`` and `forEach`, `for of` allows us to pause a loop by using await inside of the loop
+async function askMany() {
+  for(const question of questions) {
+    const answer = await ask(question);
+    console.log(answer);
+  }
+}
+
+askMany();
+*/
+
+// a good utility function #very useful to do the above
 async function asyncMap(array, callback) {
-  // make an array to store our results
+  // make an array to store the results
   const results = [];
-  // loop over our array
-  for (const item of array) {
+  for(const item of array) {
+    // const result = await callback(item)
+    //results.push(result); SHORTHAND BELOW using BEDMAS
     results.push(await callback(item));
   }
-  // when we are done the loop, return it!
+  // when we are done with the loop, return it
   return results;
 }
 
@@ -106,11 +171,3 @@ async function go() {
 
 go();
 
-// async function askMany() {
-//   for (const question of questions) {
-//     const answer = await ask(question);
-//     console.log(answer);
-//   }
-// }
-
-// askMany();
